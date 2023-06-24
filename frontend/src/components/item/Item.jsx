236c329a5ee673bde25ec40ad4c3d07da05d04a3
Rodrigo Laviani <rodrigo.laviani@naranja.com>
2023-06-24 10:35:42 -0300

    feat: add result view

--- frontend/src/components/item/Item.jsx
+++ frontend/src/components/item/Item.jsx
@@ -1,6 +1,19 @@
-const Item = ({ image, currency, amount, title, location }) => {
+import './Item.scss';
+import image2 from './../../shared/assets/Logo_ML.png';
+import shippingImg from './../../shared/assets/ic_shipping.png';
+
+const Item = ({ image, price, title, location }) => {
     return(
-        <>
-        </>
+        <article className='item-container'>
+            <img src={image2} alt="Imagen" className='item-image'></img>
+            <div className='item-data'>
+                <span className='item-price'>$ 1.980 <img src={shippingImg} alt='Shipping'></img></span>
+                <span className='item-description'>Apple Ipod Touch 5g 16gb Negro Igual A Nuevo
+                    Completo Unico!</span>
+            </div>
+            <label className='item-location'>Capital Federal</label>
+        </article>
     )
 }
+
+export default Item;
--- frontend/src/components/searchResult/SearchResult.jsx
+++ frontend/src/components/searchResult/SearchResult.jsx
@@ -1,8 +1,24 @@
 import './SearchResult.scss';
+import Item from '../item/Item';
 
 const SearchResult = ({ items }) => {
   return (
       <>
+        {/* {
+          items.map((item, index) => 
+            <Item 
+              key={`item-${index}`}
+              image={item.image}
+              price={item.price}
+              title={item.title}
+              location={item.location}
+            ></Item>
+          )
+        } */}
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
+        <Item></Item>
       </>
   );
 };
