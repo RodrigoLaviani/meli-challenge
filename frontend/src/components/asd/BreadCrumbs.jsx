7d54b5076e006972d21d9c57a545f4d1be77df76
Rodrigo Laviani <rodrigo.laviani@naranja.com>
2023-06-25 04:10:18 -0300

    copy folder

---
+++ frontend/src/components/asd/BreadCrumbs.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Breadcrumbs.scss';
+import chevron from '../../shared/assets/chevron.svg';
+
+const Breadcrumbs = ({ categories }) => {
+    const showBreadcrumbs = () => {
+        const isLastOne = (index) => categories.length === index + 1
+        return categories.map((bc, index) => (
+            <div key={`breacrumbs-${index}`}>
+                <label className={`breacrumbs-page ${isLastOne(index) && 'breacrumbs-bold'}`}>{bc}</label>
+                { !isLastOne(index) && <img className="breadcrumbs-separation-space" src={chevron} alt={'Flecha'}></img>}
+            </div>
+        ))
+    }
+
+    return (
+        <section className="breadcrumbs-container">
+            <div className='breacrumbs'>
+                {
+                    showBreadcrumbs()
+                }
+            </div>
+        </section>
+    )
+}
+
+export default Breadcrumbs;
